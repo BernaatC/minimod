c886c31f219caf7ea2aa2e96daacf19f7a38740d
BernaatC <bernat.cortes.gibert@gmail.com>
2026-03-22 11:32:23 +0100

    Add files via upload

--- minimod.h
+++ minimod.h
@@ -1,172 +1,127 @@
 #ifndef MINIMOD_H
+#define MINIMOD_H
 
 #include <stdint.h>
-#include <stdlib.h>
 
-// 2^63
-#define P63 9223372036854775808
 
-#define LOWER(X) (X & (uint64_t)UINT32_MAX)
-#define HIGHER(X) (X >> 32)
-#define OVERFLOW(X, Y) ((X > UINT64_MAX - Y) ? 1 : 0)
-
-void shl128(uint64_t* higher, uint64_t* lower) {
-	(*higher) = (*higher) << 1;
-	if ((*lower) >= P63) {
-		(*higher)++;
-	}
-	(*lower) = (*lower) << 1;
+int isCongruent(const uint64_t a, const uint64_t b, const uint64_t m) {
+	return ((a % m) == (b % m));
 }
 
-void shr128(uint64_t* higher, uint64_t* lower) {
-	(*lower) = (*lower) >> 1;
-	if ((*higher) % 2 == 1) {
-		(*lower) += P63;
-	}
-	(*higher) = (*higher) >> 1;
+uint64_t addMod(const uint64_t a, const uint64_t b, const uint64_t m) {
+	uint64_t ap = a % m, bp = m - (b % m);
+	return ((ap > bp) ? ap - bp : m - (bp - ap));
 }
 
-
-#define LINKED_LIST_NODE struct LinkedListNode
-typedef struct LinkedListNode{
-	uint64_t val;
-	LINKED_LIST_NODE* prev;
-	LINKED_LIST_NODE* next;
-};
-
-#define LINKED_LIST struct LinkedList
-typedef struct LinkedList {
-	LINKED_LIST_NODE* first;
-	LINKED_LIST_NODE* last;
-};
-
-LINKED_LIST newList() {
-	LINKED_LIST list = { 0, 0 };
-	return list;
-}
-
-void append(LINKED_LIST* self, const uint64_t newValue) {
-	if (self->first != 0) {
-		self->last->next = (LINKED_LIST_NODE*)malloc(sizeof(LINKED_LIST_NODE));
-		self->last->next->prev = self->last;
-		self->last = self->last->next;
-	}
-	else {
-		self->first = (LINKED_LIST_NODE*)malloc(sizeof(LINKED_LIST_NODE));
-		self->first->prev = 0;
-		self->last = self->first;
-	}
-	self->last->val = newValue;
-	self->last->next = 0;
-}
-
-void clearList(LINKED_LIST* self) {
-	if (self->first != 0) {
-		LINKED_LIST_NODE* iterator = self->first;
-		while (iterator->next != 0) {
-			LINKED_LIST_NODE* last = iterator;
-			iterator = iterator->next;
-			free(last);
-		}
-		free(iterator);
-		self->first = 0;
-		self->last = 0;
-	}
-}
-
-uint64_t length(const LINKED_LIST list) {
-	uint64_t length = 0;
-	LINKED_LIST_NODE* iterator = list.first;
-	while (iterator != 0) {
-		length++;
-		iterator = iterator->next;
-	}
-	return length;
-}
-
-uint64_t subMod(const uint64_t a, const uint64_t b, const uint64_t m) {  // (a - b) % m
+uint64_t subMod(const uint64_t a, const uint64_t b, const uint64_t m) {
 	uint64_t ap = a % m, bp = b % m;
-	if (bp > ap) {
-		return m - (bp - ap);
-	}
-	else {
-		return (ap - bp);
-	}
+	return ((bp > ap) ? (m - (bp - ap)) : (ap - bp));
 }
 
-uint64_t addMod(const uint64_t a, const uint64_t b, const uint64_t m) {  // (a + b) % m
-	uint64_t bp = m - (b % m);
-	return subMod(a, bp, m);
-}
-
-uint64_t mulMod(const uint64_t a, const uint64_t b, const uint64_t m) {  // (a * b) % m
-	uint64_t ll = LOWER(a) * LOWER(b), lh = LOWER(a) * HIGHER(b), hl = HIGHER(a) * LOWER(b), hh = HIGHER(a) * HIGHER(b);
-	uint64_t higher = hh + HIGHER(lh) + HIGHER(hl), lower;
-	lower = LOWER(lh) + LOWER(hl) + HIGHER(ll);
-	higher += HIGHER(lower);
-	lower = (lower << 32) + LOWER(ll);
-	
-	if (higher > 0) {
-		uint64_t modHigher = 0, modLower = m;
-		while (higher > modHigher && modHigher < P63) {
-			shl128(&modHigher, &modLower);
+uint64_t mulMod(const uint64_t a, const uint64_t b, const uint64_t m) {
+	uint64_t ap = a % m, bp = b, res = 0;
+	while (bp > 0) {
+		if (bp & 1) {
+			res = addMod(res, ap, m);
 		}
-		if (higher > modHigher) {
-			higher = higher - modHigher;
-		}
-		while (higher > 0) {
-			if (modHigher < higher || ((modHigher == higher) && (modLower < lower))) {
-				higher = higher - modHigher - ((modLower > lower) ? 1 : 0);
-				lower = lower - modLower;
-			}
-			shr128(&modHigher, &modLower);
-		}
+		ap = ((ap < 9223372036854775808) ? (ap << 1) : (ap - (m - ap))) % m;  // 2^63
+		bp = bp >> 1;
 	}
-	return (lower % m);
+	return res;
 }
 
-uint64_t invMod(const uint64_t a, const uint64_t m) {  // (a ^ -1) % m
-	LINKED_LIST quotients = newList();
-	uint64_t x = m, y = a % m, result = m;
+uint64_t invMod(const uint64_t a, const uint64_t m) {
+	uint64_t x = m, y = a % m;
 	uint64_t r = x % y;
+	uint64_t pPrev = 0, pCurr = 1, aux;
+	int neg = 0;
 	while (r != 0) {
-		append(&quotients, (x - r) / y);
+		aux = pCurr;
+		pCurr = ((x / y) * pCurr) + pPrev;
+		pPrev = aux;
+		neg = !neg;
 		x = y;
 		y = r;
 		r = x % y;
 	}
-	if (y == 1) {
-		LINKED_LIST pList = newList();
-		append(&pList, 0);
-		append(&pList, 1);
-		LINKED_LIST_NODE* iterator = quotients.first;
-		while (iterator != 0) {
-			append(&pList, (iterator->val * pList.last->val) + pList.last->prev->val);
-			iterator = iterator->next;
-		}
-		uint64_t value = pList.last->val, len = length(pList);
-		clearList(&pList);
-		result = ((len % 2 == 0) ? value : m - value);
+	if (y > 1) {
+		return m;
 	}
-	clearList(&quotients);
-	return result;
+	return (neg ? (m - pCurr) : pCurr);
 }
 
-uint64_t divMod(const uint64_t a, const uint64_t b, const uint64_t m) {  // (a / b) % m
+uint64_t divMod(const uint64_t a, const uint64_t b, const uint64_t m) {
 	uint64_t binv = invMod(b, m);
 	return ((binv == m) ? m : mulMod(a, binv, m));
 }
 
-uint64_t powMod(const uint64_t a, const uint64_t b, const uint64_t m) {  // (a ^ b) % m
+uint64_t powMod(const uint64_t a, const uint64_t b, const uint64_t m) {
 	uint64_t bp = b;
 	uint64_t result = 1;
 	for (uint64_t i = 0; i < 64; i++) {
-		uint64_t currentBit = ((bp >= P63) ? 1 : 0);
-		if (currentBit == 1) {
+		if (bp >= 9223372036854775808) {  // 2^63
 			result = mulMod(result, a, m);
 		}
 		if (i < 63 && result != 1) {
 			result = mulMod(result, result, m);
+		}
+		bp = bp << 1;
+	}
+	return result;
+}
+
+
+int isCongruent32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	return ((a % m) == (b % m));
+}
+
+uint32_t subMod32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	uint32_t ap = a % m, bp = b % m;
+	return ((bp > ap) ? (m - (bp - ap)) : (ap - bp));
+}
+
+uint32_t addMod32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	return (uint32_t)(((uint64_t)a + (uint64_t)b) % m);
+}
+
+uint32_t mulMod32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	return (uint32_t)(((uint64_t)a * (uint64_t)b) % m);
+}
+
+uint32_t invMod32(const uint32_t a, const uint32_t m) {
+	uint32_t x = m, y = a % m;
+	uint32_t r = x % y;
+	uint32_t pPrev = 0, pCurr = 1, aux;
+	int neg = 0;
+	while (r != 0) {
+		aux = pCurr;
+		pCurr = ((x / y) * pCurr) + pPrev;
+		pPrev = aux;
+		neg = ~neg;
+		x = y;
+		y = r;
+		r = x % y;
+	}
+	if (y > 1) {
+		return m;
+	}
+	return (neg ? (m - pCurr) : pCurr);
+}
+
+uint32_t divMod32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	uint32_t binv = invMod32(b, m);
+	return ((binv == m) ? m : mulMod32(a, binv, m));
+}
+
+uint32_t powMod32(const uint32_t a, const uint32_t b, const uint32_t m) {
+	uint32_t bp = b;
+	uint32_t result = 1;
+	for (uint32_t i = 0; i < 32; i++) {
+		if (bp >= 2147483648) {  // 2^31
+			result = mulMod32(result, a, m);
+		}
+		if (i < 31 && result != 1) {
+			result = mulMod32(result, result, m);
 		}
 		bp = bp << 1;
 	}
